ae7fd2d2cd7c30ba8733dbefd580098bb0379474
rubinettiStefano <stefano.rubinetti@gmail.com>
2023-11-22 15:56:54 +0100

    prima versione

---
+++ library/Console.js
@@ -0,0 +1,30 @@
+let readline = require("readline-sync");
+
+
+
+function readString()
+{
+     return readline.question("");
+}
+
+function  readInt()
+{
+     let res = parseInt(readline.question(""));
+
+     if(isNaN(res))
+          throw "Non hai inserito un numero, il programma è crashato";
+}
+function readDouble()
+{
+     let res = parseFloat(readline.question(""));
+     if(isNaN(res))
+          throw "Non hai inserito un numero, il programma è crashato";
+}
+
+function print(message)
+{
+     console.log(message);
+}
+
+
+module.exports = {readString:readString,readInt:readInt,readDouble:readDouble,print:print};
