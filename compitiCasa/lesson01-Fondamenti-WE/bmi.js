f328ea04f80213b6dfb1d8ff7f800a0ebf3de862
alieksieyeva <ira.allekseeva@gmail.com>
2023-11-23 16:20:11 +0100

    completo di esercizi weekend

---
+++ compitiCasa/lesson01-Fondamenti-WE/bmi.js
@@ -0,0 +1,13 @@
+//importare terminal
+
+//Scrivere un programma che calcoli il BMI(indice di massa corporea) dell'individuo e produca un messaggio che ha la seguente forma:
+//"Sig. John Smith di anni 30, bmi: 22"
+
+//vi serviranno i dati di input, che saranno diversi per ogni persona:
+//nome, cognome, età, altezza(cm), peso(kg)
+
+//il calcolo del bmi segue questa formula: peso(kg)/altezza(m)^2
+//fate in modo di fare la conversione da centimetri a metri
+
+
+//come output vogliamo una String con il risultato, che verrà stampata dal programma
---
+++ compitiCasa/lesson01-Fondamenti-WE/vacanza.js
@@ -0,0 +1,12 @@
+//importare il terminale
+
+//Vogliamo un programma che calcoli il prezzo di una vacanza, compreso biglietto e pernottamento
+
+
+//il programma avrà bisogno di informazioni quali
+//città di partenza, città di arrivo, kilometraggio del viaggio, costo di benzina al km, costo di pernottamento a notte e numero notti
+
+//una volta raccolto l'input il programma dovrebbe calcolare il costo del viaggio, il costo del soggiorno e costo totale
+// e produrre un messaggio come il dato di output.
+//il messaggio avrebbe il seguente aspetto:
+// Viaggio da Torino a Milano: 45 euro, pernottamento 3 giorni: 100 euro, il costo totale: 145 euro
