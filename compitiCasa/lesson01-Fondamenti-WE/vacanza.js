f96cb887d372059a966d67a48c9d1bfa52e7dfeb
rubinettiStefano <stefano.rubinetti@gmail.com>
2023-11-23 16:23:57 +0100

    v fin

--- compitiCasa/lesson01-Fondamenti-WE/vacanza.js
+++ compitiCasa/lesson01-Fondamenti-WE/vacanza.js
@@ -3,10 +3,17 @@
 //Vogliamo un programma che calcoli il prezzo di una vacanza, compreso biglietto e pernottamento
 
 
-//il programma avrà bisogno di informazioni quali
-//città di partenza, città di arrivo, kilometraggio del viaggio, costo di benzina al km, costo di pernottamento a notte e numero notti
+//il programma avrà bisogno delle seguenti informazioni, quali:
+//città di partenza
+//città di arrivo
+//kilometraggio del viaggio
+//costo di benzina al km
+//costo di pernottamento a notte 
+//numero notti
 
-//una volta raccolto l'input il programma dovrebbe calcolare il costo del viaggio, il costo del soggiorno e costo totale
-// e produrre un messaggio come il dato di output.
-//il messaggio avrebbe il seguente aspetto:
-// Viaggio da Torino a Milano: 45 euro, pernottamento 3 giorni: 100 euro, il costo totale: 145 euro
+//una volta raccolti gli input il programma dovrà calcolare:
+//costo del viaggio
+//costo del soggiorno
+//costo totale (viaggio + soggiorno)
+//Dovrà produrre e poi stampare un messaggio con il seguente aspetto:
+//Viaggio da Torino a Milano: 45 euro, pernottamento 3 giorni: 100 euro, il costo totale: 145 euro
