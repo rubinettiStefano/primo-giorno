182aa1d0162ff88901081c4e5b3e2db2ddecf0c8
rubinettiStefano <stefano.rubinetti@gmail.com>
2023-11-23 15:53:17 +0100

    quasi finale

--- lessons/lesson01-Fondamenti/01D-lesson-input.js
+++ lessons/lesson01-Fondamenti/01D-lesson-input.js
@@ -1,1 +1,25 @@
 Terminal = require("../../library/Console");
+
+//Input
+let name;
+let surname;
+let dob;
+
+//Output
+let age;
+let presentation;
+
+//Non ha senso che gli INPUT vengano scritti a mano dal programmatore
+// dovranno essere forniti dall'utente tramite tastiera
+name    =   Terminal.readString() ; 
+//Ora il programma sta aspettando che l'utente scriva una riga sul terminale
+//nel momento in cui premerà invio la riga verrà salvata all'interno della variabile name
+surname =   Terminal.readString();//come sopra
+dob     =   Terminal.readInt();//come sopra, ma il valore verrà visto come un numero e non come un testo
+//1995 come numero NON HA NULLA A CHE FARE con "1995" come testo
+
+age = 2023-dob;
+
+presentation = "Mi chiamo "+name+" "+surname+" e ho "+age+" anni";
+
+Terminal.print(presentation);
--- lessons/lesson01-Fondamenti/01D-yourTurn.js
+++ lessons/lesson01-Fondamenti/01D-yourTurn.js
@@ -1,1 +1,3 @@
 Terminal = require("../../library/Console");
+
+//Riscrivete il programma 01c-yourTurn prendendo gli input da tastiera invece che scriverli nel codice
